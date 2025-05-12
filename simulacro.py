8235c6342230990963aa2359f87d50beb1b16413
Jose Fernando Ospina <jfernando2266@gmail.com>
2025-05-12 10:03:24 -0500

    Update simulacro.py

--- simulacro.py
+++ simulacro.py
@@ -1,19 +1,18 @@
 #gestion de inventario de la libreria, seguimiento preciso de titulos, cantidad, precios, actualizados, calcular valor total del inventario
-
 inventory = [
-    {"title": "100 años de soledad", "price": 10.0, "quantity": 10},
-    {"title": "La Va llena", "price": 15.0, "quantity": 50},
-    {"title": "superacion personal", "price": 20.0, "quantity": 30},
-    {"title": "spiderman","price": 25.0, "quantity": 10},
-    {"title": "La culpa es de la vaca", "price": 30.0, "quantity": 5}
+    {"product": "book", "price": 10.0, "quantity": 10},
+    {"product": "mouse", "price": 15.0, "quantity": 50},
+    {"product": "keyboard", "price": 20.0, "quantity": 30},
+    {"product": "monitor","price": 25.0, "quantity": 10},
+    {"product": "headset", "price": 30.0, "quantity": 5}
 ]
 
+#creation of functions, and definition of functions
 
-#funtions
-# option validation
+#validation functions and messages
 def confirmation(valor):
     try:
-        return 0 < float(valor) < 6
+        return 0 < float(valor) < 6 
     except ValueError:
         return False
 #int validation
@@ -23,6 +22,7 @@ def numint(valor):
         return True
     except ValueError:
         return False
+    
 #float valitation
 def numfloat(valor):
     try:
@@ -30,14 +30,15 @@ def numfloat(valor):
         return True
     except ValueError:
         return False
+    
 #menssage validation 
 def answer():
-    while True:
-        answer = input("Would you like to continue? (Yes/No): ").strip().lower()
-        if answer in ["Yes"] or ['no']:
-            return answer 
+     while True:
+        answer = input("Do you want to continue(Yes/No): ").strip().lower()
+        if answer in ["yes"] or ["no"]:
+            return answer
         else:
-            print("Invalid answer. Please enter 'yes' or 'no'.")
+            print("Error, please enter a correct answer 'Yes' o 'No'.")
 #price validation
 def pricevalidation(price):
      while True:
@@ -46,130 +47,145 @@ def pricevalidation(price):
          elif numfloat(price) or float(price) >=0:
                  print("price successfully entered and registered")
                  break
-#add book 
-def add(title,price,quantity):
-    inventory.append({"name":title, "price": float(price), "quantity" : int(quantity)})
-    print(f"Book {title}, successfully added.")
-        
-#search books
-def search(title):
-    if title not in inventory:
-        print("this book is not registered")
-    else:
-        price=inventory[title]["price"]
-        quantity=inventory[title]["quantity"]
-        print(f"Book: {title} | price: {price:.2f} | quantity: {quantity}")   
 
-#update price
-def updateprice(title, newprice,newquantity):
-    if title not in inventory:
-        print("This product is not in stock")
-    else:
-        inventory[title]["price"]=newprice
-        inventory[title]["quantity"]=newquantity
-        print(f"The price of the book {title} was change to {newprice:.2f} and the quantity is {newquantity}")
-    
-#remove books
-def Removebook(title):
-    if title not in inventory:
-        print("this book is not registered in the inventory")
-    else:
-        inventory.pop(title)
-        print(f"the book was removed {title}of the inventory")
+#add products function / In this part I create a function that allows us to add the products to our list and dictionary.
+
+def add(product,price,quantity):
+    inventory.append({"product":product, "price": price, "quantity" : int(quantity)})
+    print(f"product {product}, successfully added.")
         
-#total value in inventory 
+#search products funtion   /   This function will allow us to search for the product in our list and dictionary with the name and specific information.      
+def search(product):
+    if product not in inventory:
+        print("this product is not registered")
+    else:
+        price=inventory[product]["price"]
+        quantity=inventory[product]["quantity"]
+        print(f"product: {product} , price: {price:.2f} | quantity: {quantity}") 
+        
+
+# Update the price of an existing product funtion / This function will allow us to change the existing price for a new one.
+def updated_price(product, new_price):
+    if product not in inventory:
+        print("This product is not in inventory")
+    else:
+        inventory[product]["price"]=new_price
+        print(f"The price of the product {product} has been updated to {new_price:.2f}")
+        
+#function to delete a product / With this function we can search for a product in the list and delete it completely. For this we use the (.pop)
+def remove(product):
+    if product not in inventory:
+        print("This product is not in inventory")
+    else:
+        inventory.pop(product)
+        print(f"The product{product} has been removed from inventory.")
+        
+        
+# funtion to calculate the total value of the inventory / In this function we create a lambda function to add and map the items in the inventory and multiply the price by the quantity of products.
 def totalvalue():
     if not inventory:
-        print("Inventory is empty.")
+        print("The inventory is empty.")
     else:
-        totalvalue = sum(map(lambda item: item["price"] * item["quantity"], inventory.values()))
-        print(f"The total value of the inventory is: ${totalvalue:.2f}")
-        
-#menu
-def menu():
-    print("Welcome to inventory System")
-    print("select the desired option")
-    print("1. Add Books")
-    print("2.Search Books")
-    print("3.Update Price")
-    print("4. Remove Books in inventory")
-    print("5.Total value in inventory")
-    print("6.Sign out")
+        total_value = sum(map(lambda item: item["precio"] * item["cantidad"], inventory.values()))
+        print(f"The total value of the inventory is: ${total_value:.2f}")
 
-    
-#Run System
+# Show main menu / In this part we define the menu as a function to be able to do it in a practical way and to be able to call the menu at any time that is needed with its function.
+def menu():
+    print("Welcome to the inventory system")
+    print("\n Inventory Management System")
+    print("1. Add products")
+    print("2. Consult product")
+    print("3. Update price")
+    print("4. Remove products")
+    print("5. Calculate total inventory value")
+    print("6. Log out")
+
+# Function of executing the system /  In this function we make the steps and the unification of the code, this is where we implement all the functions and give meaning to the code.
 def system():
+    #here I enter the code in a while loop to create the loop and avoid breaking the code.
     while True:
         menu()
-        option=input("Please write the number of the action you wish to perform").title()
-        if confirmation(option)== False:
-            print("Error, you must enter a number between 1 to 6")
-        if option=="1":
+        option = input("Select an option(1-6): ")
+        if confirmation(option) == False:
+            print("Error, option must be a number between 1 and 6.")
+        
+            #I add the conditions from the first option and again enter everything in a while loop to avoid code fractures.
+        if option == "1":
             while True:
                 while True:
-                    title= input("Please enter the name of the book you want to add").title()
-                    if title not in inventory:
-                        print("The registration was correct")
+                    product=input("Enter the Product Name :").lower()
+                    if product not in inventory:
+                        print("successfully registered")
                         break
                     else:
-                        print(f"This book is already registered in the inventory",title)
-                price= float(input("Please enter the price of the book"))
+                        print(f"This product is already registered,{product}")
+                    
+                price=input("Enter the value of the product :").strip()
                 while True:
                     if not numfloat (price) or float(price) <= 0:
-                        print("Error el numero debe de ser un numero decimal positivo.")
-                            
-                    elif numfloat(price) or float(price) >=0:
-                        print("Precio ingresado y registrado con exito")
+                        print("error the number must be a positive decimal number.")
+                        
+                    elif numfloat(price) or float(price) >0:
+                        print("price entered and recorded successfully")
                         break
-                quantity=int(input("Please enter the quantity of books"))
+                quantity=input("Enter the quantity of products: ").strip()
                 while True:
-                    if not numint(quantity) or int(quantity)<=0:
-                        print("Error, Enter a valid amount")
-                            
-                    elif numint(quantity) or int(quantity) >0:
-                        print("Amount successfully entered and registered")
-                    add(title,price,quantity)
-                    print(inventory)
-                    if answer()== "No":
+                    if not numint(quantity) or int (quantity) <0:
+                        print("Error, the amount must be a positive integer.")
+                        
+                    elif numint(quantity) or int (quantity) >0:
+                        print("Amount entered successfully")
                         break
+                add(product,price,quantity)
+                print(inventory)
+                if answer()== "no" :
+                    break
+        #I add the second condition and use the search function.
         elif option =="2":
-             title=input("Enter the name of the book").title()
-             search(title)
-        elif option=="3":
+             product=input("Enter the name of the product: ").lower()
+             search(product)
+            
+            #I set the third condition and validate that the data entry is correct, in this case a float type number.
+        elif option == "3":
             while True:
-                title=input("Enter the name of the book you wish to change the price of") 
-                if title not in inventory:
-                   print("This product is not in stock")  
-                   return  
-                newprice=float(input("Ingrese el nuevo precio: "))
-                while True:
-                    if not numint(newprice) or float(newprice) <= 0:
-                        print("Error, the price must be a positive decimal number..")
-                        return
-                    elif numfloat(newprice) or float(newprice) >=0:
-                        print("price successfully entered and registered")
-                        break
-                updateprice(title, newprice)
-                if answer()== "Yes" :
-                    break
-        elif option=="4":
-            while True:
-                title=input("Enter the name of the Book to delete: ").lower()
-                if title not in inventory:
-                    print("This product is not in stock")
+                product=input("Enter the name of the product to update: ").lower()
+                if product not in inventory:
+                    print("This product is not registered in inventory")
                     return
-                print(f"The Book{title} was removed.")
-                Removebook(title) 
-                if answer()== "Yes" :   
+                print(f"Product: {product} | Price: {inventory[product]['price']:.2f} | quantity: {inventory[product]["quantity"]}")
+                while True:
+                    new_price=float(input("Enter the new price: "))
+                    if not numfloat(new_price) or float(new_price) <= 0:
+                        print("Error, the price must be a positive decimal number.")
+                        return
+                    elif numfloat(new_price) or float(new_price) >0:
+                        print("price entered and recorded successfully")
+                        break
+                updated_price(product, new_price)
+                if answer()== "yes" :
                     break
-                
-        elif option=="5":
+              #  I added the 4th condition to remove products, here I used the remove function and a response validation.
+        elif option == "4":
+            while True:
+                product=input("Enter the name of the product to be deleted: ").lower()
+                if product not in inventory:
+                    print("This product is not in inventory")
+                    return
+                print(f"The product{product} has been removed from inventory.")
+                remove(product) 
+                if answer()== "yes" :   
+                    break
+                # I use the 5th function to calculate the total value, I just call the function
+        elif option == "5":
             totalvalue()
-        elif option=="6":
+            
+            #In this last option, the rest closes the code and thus can break the complete cycle of this, and close the system.
+        elif option == "6":
             print("Leaving the program. See you soon!")
             break
         else:
-            print("Invalid option. Try again.")
+            print("Error, Invalid option. Please try again.")
+    
 system()
             
                 
